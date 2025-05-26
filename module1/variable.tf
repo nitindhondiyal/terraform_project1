36fb890053031628a1a8a23d5981ba5bcb2e7b94
nitindhondiyal <58582907+nitindhondiyal@users.noreply.github.com>
2025-05-27 01:53:50 +0530

    Update variable.tf

--- module1/variable.tf
+++ module1/variable.tf
@@ -1,9 +1,11 @@
 variable "ami_value" {
     description = " value of the ami"
+    type = string
   
 }
 
 variable "instance_type" {
     description = " value of instance type "
+    type = string
   
 }
